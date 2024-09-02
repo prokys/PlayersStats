156668ed513dec12554038c517361f3b071d0d5a
Prokop Ferdan <prokes1@gmail.com>
2024-09-02 17:42:45 +0200

    addded getters, setters and constructors to Player class, updated endpoint /players to show players-list template

--- src/main/java/com/prokys/PlayersStats/controller/PlayersController.java
+++ src/main/java/com/prokys/PlayersStats/controller/PlayersController.java
@@ -1,15 +1,31 @@
 package com.prokys.PlayersStats.controller;
 
+import com.prokys.PlayersStats.entity.Player;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/players")
 public class PlayersController {
 
     @GetMapping("/")
-    public String baseMapping(){
-        return "Hello";
+    public String listPlayers(Model model){
+        List<Player> players = new ArrayList<>();
+        LocalDate date = LocalDate.of(2024,1,2);
+
+        players.add(new Player("ASDAS", "ADSD", "asdads@gmail.com", date));
+        players.add(new Player("Test", "Test", "Test@gmail.com", date));
+
+
+        model.addAttribute("players", players);
+
+        return "players-list";
     }
 }
--- src/main/java/com/prokys/PlayersStats/entity/Player.java
+++ src/main/java/com/prokys/PlayersStats/entity/Player.java
@@ -1,13 +1,16 @@
 package com.prokys.PlayersStats.entity;
 
 import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
 @Table(name = "players")
 public class Player {
 
+    // object fields
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     @Id
@@ -23,6 +26,59 @@ public class Player {
     private String email;
 
     @Column(name = "date_of_birth")
-    private Date dateOfBirth;
+    private LocalDate dateOfBirth;
 
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // constructors
+
+    public Player() {
+    }
+
+    public Player(String firstName, String lastName, String email, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
 }
