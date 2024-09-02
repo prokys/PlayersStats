6005799557a4a652f1fad346fe823795e7f8dfec
Prokop Ferdan <prokes1@gmail.com>
2024-09-02 15:43:57 +0200

    initializing project

---
+++ src/main/java/com/prokys/PlayersStats/PlayersStatsApplication.java
@@ -0,0 +1,13 @@
+package com.prokys.PlayersStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prokys/PlayersStats/PlayersStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prokys.PlayersStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
