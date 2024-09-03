257b7c4f457bdebcc6ee324d511698e40623ce2d
Prokop Ferdan <prokes1@gmail.com>
2024-09-03 09:07:45 +0200

    added PlayersDAO and PlayersDAOImpl with findPLayers() method

---
+++ src/main/java/com/prokys/PlayersStats/dao/PlayersDAO.java
@@ -0,0 +1,10 @@
+package com.prokys.PlayersStats.dao;
+
+import com.prokys.PlayersStats.entity.Player;
+
+import java.util.List;
+
+public interface PlayersDAO {
+
+    List<Player> findPlayers();
+}
---
+++ src/main/java/com/prokys/PlayersStats/dao/PlayersDAOImpl.java
@@ -0,0 +1,34 @@
+package com.prokys.PlayersStats.dao;
+
+import com.prokys.PlayersStats.entity.Player;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PlayersDAOImpl implements PlayersDAO{
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // set up constructor injection
+    @Autowired
+    public PlayersDAOImpl(EntityManager manager){
+        entityManager = manager;
+    }
+
+    @Override
+    public List<Player> findPlayers() {
+
+        // create query
+        TypedQuery<Player> query = entityManager.createQuery("FROM Players", Player.class);
+
+        // execute query
+        List<Player> players = query.getResultList();
+
+        return players;
+    }
+}
